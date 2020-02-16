812942708ea2437e932612099f365b2d901abe3e
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2020-02-16 16:45:26 +0530

    customize the exception message

--- src/main/java/com/demospringboot/cms/service/CustomerService.java
+++ src/main/java/com/demospringboot/cms/service/CustomerService.java
@@ -1,11 +1,13 @@
 package com.demospringboot.cms.service;
 
 import com.demospringboot.cms.dao.CustomerDAO;
+import com.demospringboot.cms.exception.CustomerNotFoundException;
 import com.demospringboot.cms.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
@@ -29,6 +31,7 @@ public class CustomerService {
 
     public List<Customer> getCustomers()
     {
+
 //        return customerList;
         return customerDAO.findAll();
     }
@@ -37,8 +40,19 @@ public class CustomerService {
     {
 //        return customerList.stream().filter(c->c.getCustomerId() == customerId).findFirst().get();
 
-        return customerDAO.findById(customerId). // this returns the optional object
-                get();//this give the correct customer
+
+
+//        return customerDAO.findById(customerId). // this returns the optional object
+//                get();//this give the correct customer
+
+        Optional<Customer> optionalCustomer = customerDAO.findById(customerId);
+
+        if(!optionalCustomer.isPresent())
+        {
+            throw new CustomerNotFoundException(("Customer Record is not available!"));
+        }
+
+        return optionalCustomer.get();
     }
 
     public Customer updateCustomer(int customerId, Customer customer)
