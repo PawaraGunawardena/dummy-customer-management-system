cbcf44c2b15b861325efd76f0b952ed09fb778ef
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2020-02-16 12:04:03 +0530

    override finadAll method to return a list of customer object

--- src/main/java/com/demospringboot/cms/dao/CustomerDAO.java
+++ src/main/java/com/demospringboot/cms/dao/CustomerDAO.java
@@ -4,9 +4,14 @@ import com.demospringboot.cms.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerDAO extends CrudRepository<
         Customer, //type of the class dealing with
         Integer>  // id type of the entity
 {
+
+    @Override
+    List<Customer> findAll();
 }
